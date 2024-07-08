72f2a952ec143cd29a6937d91d8b4f09a6431461
Raza <bilgramiraza@yahoo.com>
2024-07-08 19:25:01 +0530

    Added Ability to Add new People to the Phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,30 @@ import { useState } from "react";
 
 function App() {
   const [people, setPeople] = useState([
-    { name: 'Aaron Smith' }
+    { name: 'Aaron Smith' },
   ]);
+
+  const [newName, setNewName] = useState('');
+
+  const handleNewNameChange = (e) => setNewName(e.target.value);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Not yet Implemented');
+    setPeople([...people, { name: newName }]);
+    setNewName('');
   }
   return <div>
     <h2>Phone Book</h2>
     <form onSubmit={handleSubmit}>
       <label>
-        Name : <input type="text" id="name" />
+        Name : <input type="text" id="name" value={newName} onChange={handleNewNameChange} />
       </label>
       <button type="submit">Add</button>
     </form>
     <h2>Numbers</h2>
-    <ul>
+    <ol>
       {people.map(person => <li key={person.name}>{person.name}</li>)}
-    </ul>
+    </ol>
   </div>;
 }
 
