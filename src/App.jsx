7902ac175122ff87b5af71c9f2c935ff150f37a1
Raza <bilgramiraza@yahoo.com>
2024-07-08 19:08:13 +0530

    Setup basic Phone form & List

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState([
+    { name: 'Aaron Smith' }
+  ]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Not yet Implemented');
+  }
   return <div>
-    <p>Hello World</p>
+    <h2>Phone Book</h2>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name : <input type="text" id="name" />
+      </label>
+      <button type="submit">Add</button>
+    </form>
+    <h2>Numbers</h2>
+    <ul>
+      {people.map(person => <li key={person.name}>{person.name}</li>)}
+    </ul>
   </div>;
 }
 
