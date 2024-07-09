55f458f4e763e18877589d53408c8c440a74ca3a
Raza <bilgramiraza@yahoo.com>
2024-07-09 06:14:57 +0530

    Added Ability to filter the phonebook by Names or/and Phone Numbers

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,24 @@ function checkDuplicates(phoneBook, name) {
 
 function App() {
   const [people, setPeople] = useState([
-    {
-      name: 'Aaron Smith',
-      number: '12-345-6789',
-    },
+    { name: 'Aaron Smith', number: '12-345-6789', id: 0 },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
 
-  const handleNewNameChange = (e) => setNewName(e.target.value);
+  const [filterName, setFilterName] = useState('');
+  const [filterNumber, setFilterNumber] = useState('');
 
+  const handleNewNameChange = (e) => setNewName(e.target.value);
   const handleNewNumberChange = (e) => setNewNumber(e.target.value);
+
+  const handleFilterNameChange = (e) => setFilterName(e.target.value);
+  const handleFilterNumberChange = (e) => setFilterNumber(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,10 +32,18 @@ function App() {
       alert(`${newName} Already Exists`);
       return;
     }
-    setPeople([...people, { name: newName, number: newNumber }]);
+    setPeople([...people, { name: newName, number: newNumber, id: people.length + 1 }]);
     setNewName('');
     setNewNumber('');
   }
+
+  const filteredPhoneBook = people.filter(({ name: personName, number: personNumber }) => {
+    const nameFilter = personName.toLowerCase().includes(filterName.toLowerCase());
+    const numberFilter = personNumber.includes(filterNumber);
+
+    return nameFilter && numberFilter;
+  });
+
   return <div>
     <h2>Phone Book</h2>
     <form onSubmit={handleSubmit}>
@@ -43,8 +57,16 @@ function App() {
     </form>
     <h2>Numbers</h2>
     <ol>
-      {people.map(person => <li key={person.name}>{person.name} : {person.number}</li>)}
+      {filteredPhoneBook.map(person => <li key={person.id}>{person.name} : {person.number}</li>)}
     </ol>
+    <form>
+      <label>
+        Filter By Name : <input type="text" id="filterName" value={filterName} onChange={handleFilterNameChange} />
+      </label>
+      <label>
+        Filter By Number : <input type="text" id="filterNumber" value={filterNumber} onChange={handleFilterNumberChange} />
+      </label>
+    </form>
   </div>;
 }
 
