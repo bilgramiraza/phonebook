d6827634ffbcee8404c9a0e2272744ba423af99c
Raza <bilgramiraza@yahoo.com>
2024-07-11 19:25:28 +0530

    Setup Simple DataFetching from 'server' using Axios and UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PhoneBookForm from "./components/PhoneBookForm";
 import FilterForm from "./components/FilterForm";
 import DisplayPhoneBook from "./components/DisplayPhoneBook";
+import axios from "axios";
 
 function App() {
-  const [people, setPeople] = useState([
-    { name: 'Aaron Smith', number: '12-345-6789', id: 0 },
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPeople(response.data);
+      })
+  }, []);
 
   const [filter, setFilter] = useState({
     name: '',
